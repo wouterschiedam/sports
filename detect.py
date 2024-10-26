4a9c87aae9215168ee45a25f03b6f414ae4bf365
wouterschiedam <79146984+wouterschiedam@users.noreply.github.com>
2024-10-26 11:47:58 +0200

    Begin siglip

--- detect.py
+++ detect.py
@@ -18,7 +18,7 @@ PLAYER_DETECTION_MODEL = load_local_model(PLAYER_DETECTION_MODEL_PATH);
 BALL_ID = 0;
 
 SOURCE_VIDEOS = [
-    "./content/08fd33_0.mp4",
+     "./content/08fd33_0.mp4",
      "./content/0bfacc_0.mp4",
      "./content/121364_0.mp4",
      "./content/2e57b9_0.mp4",
@@ -26,11 +26,11 @@ SOURCE_VIDEOS = [
 ];
 
 TARGET_VIDEO_PATH = [
-    "./content/08fd33_0_result.mp4",
-    "./content/0bfacc_0_result.mp4",
-    "./content/121364_0_result.mp4",
-    "./content/2e57b9_0_result.mp4",
-    "./content/573e61_0_result.mp4"
+     "./content/processed/08fd33_0.mp4",
+     "./content/processed/0bfacc_0.mp4",
+     "./content/processed/121364_0.mp4",
+     "./content/processed/2e57b9_0.mp4",
+     "./content/processed/573e61_0.mp4"
 ];
 
 class_names = ['ball', 'goalkeeper', 'player', 'referee'];
@@ -54,75 +54,75 @@ triangle_annotator = sv.TriangleAnnotator(
 tracker = sv.ByteTrack();
 tracker.reset()
 
+for SOURCE_VIDEO, TARGET_VIDEO in zip(SOURCE_VIDEOS, TARGET_VIDEO_PATH):
 
-video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEOS[1]);
-video_sink = sv.VideoSink(TARGET_VIDEO_PATH[1], video_info=video_info);
-frame_generator = sv.get_video_frames_generator(SOURCE_VIDEOS[1]);
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO);
+    video_sink = sv.VideoSink(TARGET_VIDEO, video_info=video_info);
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO);
+
+    with video_sink:
+        for frame in tqdm(frame_generator, total=video_info.total_frames):
+            
+            result = PLAYER_DETECTION_MODEL(frame)[0];
+
+            # Extract bounding boxes, confidence scores, and class labels from results
+            boxes = result.boxes.xyxy.cpu().numpy();  # Bounding boxes
+            confidences = result.boxes.conf.cpu().numpy();  # Confidence scores
+            class_ids = result.boxes.cls.cpu().numpy().astype(int);  # Class IDs
+            # Create Detections object
+            detections = sv.Detections(
+                xyxy=boxes,
+                confidence=confidences,
+                class_id=class_ids
+            );
+
+            
+            # Ensure class IDs and confidences are valid arrays before proceeding
+            if detections.class_id is not None and detections.confidence is not None:
+                # Filter for ball and other detections and create new Detections objects
+                ball_mask = detections.class_id == BALL_ID
+                if ball_mask.any():  # Check if there are any ball detections
+                    ball_detections = sv.Detections(xyxy=detections.xyxy[ball_mask], confidence=detections.confidence[ball_mask], class_id=detections.class_id[ball_mask]);
+                else:
+                    ball_detections = sv.Detections(xyxy=np.empty((0, 4)), confidence=np.empty(0), class_id=np.empty(0, dtype=int));
+
+                non_ball_mask = detections.class_id != BALL_ID
+                if non_ball_mask.any():  # Check if there are any non-ball detections
+                    all_detections = sv.Detections(xyxy=detections.xyxy[non_ball_mask], confidence=detections.confidence[non_ball_mask], class_id=detections.class_id[non_ball_mask]);
+                else:
+                    all_detections = sv.Detections(xyxy=np.empty((0, 4)), confidence=np.empty(0), class_id=np.empty(0, dtype=int));
 
 
-with video_sink:
-    for frame in tqdm(frame_generator, total=video_info.total_frames):
-        
-        result = PLAYER_DETECTION_MODEL(frame)[0];
+                # labels = [
+                #     f"{class_names[class_id]} {confidence:.2f}"
+                #     for class_id, confidence in zip(detections.class_id, detections.confidence)
+                # ];
 
-        # Extract bounding boxes, confidence scores, and class labels from results
-        boxes = result.boxes.xyxy.cpu().numpy();  # Bounding boxes
-        confidences = result.boxes.conf.cpu().numpy();  # Confidence scores
-        class_ids = result.boxes.cls.cpu().numpy().astype(int);  # Class IDs
-        # Create Detections object
-        detections = sv.Detections(
-            xyxy=boxes,
-            confidence=confidences,
-            class_id=class_ids
-        );
-
-        
-        # Ensure class IDs and confidences are valid arrays before proceeding
-        if detections.class_id is not None and detections.confidence is not None:
-            # Filter for ball and other detections and create new Detections objects
-            ball_mask = detections.class_id == BALL_ID
-            if ball_mask.any():  # Check if there are any ball detections
-                ball_detections = sv.Detections(xyxy=detections.xyxy[ball_mask], confidence=detections.confidence[ball_mask], class_id=detections.class_id[ball_mask]);
             else:
+                print("No labels found for frame. Using default labels...")
                 ball_detections = sv.Detections(xyxy=np.empty((0, 4)), confidence=np.empty(0), class_id=np.empty(0, dtype=int));
-
-            non_ball_mask = detections.class_id != BALL_ID
-            if non_ball_mask.any():  # Check if there are any non-ball detections
-                all_detections = sv.Detections(xyxy=detections.xyxy[non_ball_mask], confidence=detections.confidence[non_ball_mask], class_id=detections.class_id[non_ball_mask]);
-            else:
                 all_detections = sv.Detections(xyxy=np.empty((0, 4)), confidence=np.empty(0), class_id=np.empty(0, dtype=int));
 
+            ball_detections.xyxy = sv.pad_boxes(xyxy=ball_detections.xyxy, px=10)
+            all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=True)
 
-            # labels = [
-            #     f"{class_names[class_id]} {confidence:.2f}"
-            #     for class_id, confidence in zip(detections.class_id, detections.confidence)
-            # ];
+            if all_detections.class_id is not None:
+                all_detections.class_id -= 1
 
-        else:
-            print("No labels found for frame. Using default labels...")
-            ball_detections = sv.Detections(xyxy=np.empty((0, 4)), confidence=np.empty(0), class_id=np.empty(0, dtype=int));
-            all_detections = sv.Detections(xyxy=np.empty((0, 4)), confidence=np.empty(0), class_id=np.empty(0, dtype=int));
+            all_detections = tracker.update_with_detections(detections=all_detections)
 
-        ball_detections.xyxy = sv.pad_boxes(xyxy=ball_detections.xyxy, px=10)
-        all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=True)
+            if all_detections.tracker_id is not None:
+                labels = [
+                    f"#{tracker_id}"
+                    for tracker_id
+                    in all_detections.tracker_id
+                ];
+            else:
+                labels = [];
 
-        if all_detections.class_id is not None:
-            all_detections.class_id -= 1
-
-        all_detections = tracker.update_with_detections(detections=all_detections)
-
-        if all_detections.tracker_id is not None:
-            labels = [
-                f"#{tracker_id}"
-                for tracker_id
-                in all_detections.tracker_id
-            ];
-        else:
-            labels = [];
-
-        # Annotate the frame with detections
-        annotated_frame = ellipse_annotator.annotate(frame.copy(), all_detections);
-        annotated_frame = triangle_annotator.annotate(annotated_frame, ball_detections);
-        annotated_frame = label_annotator.annotate(annotated_frame, all_detections, labels=labels);
-        video_sink.write_frame(annotated_frame);
+            # Annotate the frame with detections
+            annotated_frame = ellipse_annotator.annotate(frame.copy(), all_detections);
+            annotated_frame = triangle_annotator.annotate(annotated_frame, ball_detections);
+            annotated_frame = label_annotator.annotate(annotated_frame, all_detections, labels=labels);
+            video_sink.write_frame(annotated_frame);
 
--- team-cluster.py
+++ team-cluster.py
@@ -2,6 +2,15 @@ from tqdm import tqdm
 import supervision as sv
 from ultralytics import YOLO
 import numpy as np
+import torch
+from transformers import AutoProcessor, SiglipVisionModel
+from more_itertools import chunked
+
+SIGLIP_MODEL_PATH = 'google/siglip-base-patch16-224'
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+EMBEDDINGS_MODEL = SiglipVisionModel.from_pretrained(SIGLIP_MODEL_PATH, map_location=DEVICE)
+EMBEDDINGS_PROCESSOR = AutoProcessor.from_pretrained(SIGLIP_MODEL_PATH)
 
 def load_local_model(model_path: str) -> YOLO:
     # Loads a YOLO model from a local path
@@ -11,7 +20,7 @@ PLAYER_DETECTION_MODEL_PATH = "./runs/detect/train9/weights/best.pt";
 PLAYER_DETECTION_MODEL = load_local_model(PLAYER_DETECTION_MODEL_PATH);
 
 SOURCE_VIDEOS = [
-    "./content/08fd33_0.mp4",
+     "./content/08fd33_0.mp4",
      "./content/0bfacc_0.mp4",
      "./content/121364_0.mp4",
      "./content/2e57b9_0.mp4",
@@ -34,16 +43,37 @@ for frame in tqdm(frame_generator, desc='collecting crops'):
     confidences = result.boxes.conf.cpu().numpy();  # Confidence scores
     class_ids = result.boxes.cls.cpu().numpy().astype(int);  # Class IDs
     # Create Detections object
-    
     detections = sv.Detections(
         xyxy=boxes,
         confidence=confidences,
         class_id=class_ids
     );
 
-    detections = detections.with_nms(threshold=0.5, class_agnostic=True)
-    detections = detections[detections.class_id == PLAYER_ID]
+    # Apply Non-Maximum Suppression without reassigning detections
+    nms_detections = detections.with_nms(threshold=0.5, class_agnostic=True)
 
-    players_crops = [sv.crop_image(frame, xyxy) for xyxy in detections.xyxy]
+    # Filter by class_id for PLAYER_ID directly on the numpy arrays
+    player_mask = nms_detections.class_id == PLAYER_ID
+    filtered_boxes = nms_detections.xyxy[player_mask]
 
-     
+    # Crop images based on the filtered bounding boxes
+    players_crops = [sv.crop_image(frame, xyxy) for xyxy in filtered_boxes]
+    crops += players_crops
+
+sv.plot_images_grid(crops[:100], grid_size=(10, 10))
+
+
+BATCH_SIZE = 32
+
+crops = [sv.cv2_to_pillow(crop) for crop in crops]
+batches = chunked(crops, BATCH_SIZE)
+data = []
+with torch.no_grad():
+    for batch in tqdm(batches, desc='embedding extraction'):
+        inputs = EMBEDDINGS_PROCESSOR(images=batch, return_tensors="pt").to(DEVICE)
+        outputs = EMBEDDINGS_MODEL(**inputs)
+        embeddings = torch.mean(outputs.last_hidden_state, dim=1).cpu().numpy()
+        data.append(embeddings)
+
+data = np.concatenate(data)
+
